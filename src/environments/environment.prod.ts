73b8b6bf4036b66c7aa90a725dae929d8033c318
filipwtf <filipwedzicha@gmail.com>
2020-05-22 19:08:57 +0100

    Added FireBaseAuth + Auth guard

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBxmeuatfMSUIZCr9hCjoC30_RhAYmkZ3E',
+    authDomain: 'kanban-8d0b0.firebaseapp.com',
+    databaseURL: 'https://kanban-8d0b0.firebaseio.com',
+    projectId: 'kanban-8d0b0',
+    storageBucket: 'kanban-8d0b0.appspot.com',
+    messagingSenderId: '282112568129',
+    appId: '1:282112568129:web:99e77834dfe6de14234fc0',
+    measurementId: 'G-XFXQK71078',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBxmeuatfMSUIZCr9hCjoC30_RhAYmkZ3E',
+    authDomain: 'kanban-8d0b0.firebaseapp.com',
+    databaseURL: 'https://kanban-8d0b0.firebaseio.com',
+    projectId: 'kanban-8d0b0',
+    storageBucket: 'kanban-8d0b0.appspot.com',
+    messagingSenderId: '282112568129',
+    appId: '1:282112568129:web:99e77834dfe6de14234fc0',
+    measurementId: 'G-XFXQK71078',
+  },
 };
 
 /*
