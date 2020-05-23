306c2a2c671e36efc2b1b656504553495cf918f0
filipwtf <filipwedzicha@gmail.com>
2020-05-23 18:38:48 +0100

    :collision: Publicshed site + :page_with_curl: UI Update

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-shell',
@@ -9,12 +10,15 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent {
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public afAuth: AngularFireAuth
+  ) {}
+
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe([Breakpoints.Handset])
     .pipe(
       map((result) => result.matches),
       shareReplay()
     );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
 }
